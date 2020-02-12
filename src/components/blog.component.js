0135ca71933d43a5279825be08bcfb2495660883
Kaj Białas <kaj.bialas@dev-industry.com>
2020-02-12 04:14:24 +0100

    Final solution of useReducer

--- src/components/blog.component.js
+++ src/components/blog.component.js
@@ -1,42 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/1';
 
+const INITIAL_STATE = {
+  isLoading: false,
+  isError: false,
+  data: {},
+};
+
+function blogReducer(state, action) {
+  switch (action.type) {
+    case 'SET_LOADING':
+      return {
+        isLoading: true,
+        isError: false,
+        data: action.value
+      };
+    case 'SET_ERROR':
+      return {
+        isLoading: false,
+        isError: true,
+        data: {}
+      };
+    case 'SET_DATA':
+      return {
+        isLoading: false,
+        isError: false,
+        data: action.value
+      };
+  }
+}
 function Blog() {
-  const [blogPost, setBlogPost] = useState({});
-  const [loadStatus, setLoadStatus] = useState({
-    isLoading: true,
-    isError: true,
-  });
+  const [blogPost, dispatch] = useReducer(blogReducer, INITIAL_STATE);
 
   useEffect(() => {
-    setLoadStatus({
-      isLoading: true,
-      isError: false,
-    });
+    dispatch({type: 'SET_LOADING'});
 
     fetch(API_ENDPOINT)
       .then(response => response.json())
       .then(data => {
-        setBlogPost({...data});
-        setLoadStatus({
-          isLoading: false,
-          isError: false,
-        });
+        dispatch({type: 'SET_DATA', value: {...data}});
       })
-      .catch(() => setLoadStatus({
-        isLoading: false,
-        isError: true,
-      }));
+      .catch(() => dispatch( {type:'SET_ERROR'}));
   }, []);
 
   return (
     <div>
-      {loadStatus.isLoading
+      {blogPost.isLoading
         ? <div>Loading...</div>
         : <div>
-          <h1>{blogPost.title}</h1>
-          <div>{blogPost.body}</div>
+          <h1>{blogPost.data.title}</h1>
+          <div>{blogPost.data.body}</div>
         </div> }
     </div>
   )
